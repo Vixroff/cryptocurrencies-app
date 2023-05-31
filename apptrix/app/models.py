b988c971a71baac888b022681f867697c2073f8c
Maksim Vikhrov <maxvihr@yandex.ru>
2023-05-31 12:42:58 +0300

    Initial commit; Create models; Docker

---
+++ apptrix/app/models.py
@@ -0,0 +1,62 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CryptoCurrency(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    name = models.CharField(
+        verbose_name='Название'
+    )
+    symbol = models.CharField(
+        verbose_name='Символ'
+    )
+    price = models.DecimalField(
+        max_digits=20,
+        decimal_places=10,
+        verbose_name='Цена',
+    )
+    market_cap = models.FloatField(
+        verbose_name='Капитализация',
+        null=True,
+        blank=True,
+    )
+    volume_24h = models.FloatField(
+        verbose_name='Объем торгов за сутки',
+        null=True,
+        blank=True,
+    )
+    percent_change_1h = models.DecimalField(
+        max_digits=10,
+        decimal_places=5,
+        verbose_name='Изменение за час',
+        null=True,
+        blank=True,
+    )
+    percent_change_24h = models.DecimalField(
+        max_digits=10,
+        decimal_places=5,
+        verbose_name='Изменение за сутки',
+        null=True,
+        blank=True,
+    )
+    percent_change_7d = models.DecimalField(
+        max_digits=10,
+        decimal_places=5,
+        verbose_name='Изменение за неделю',
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'Криптовалюта'
+        unique_together = ('name', 'symbol')
+
+
+class User(AbstractUser):
+    cryptocurrencies = models.ManyToManyField(CryptoCurrency)
